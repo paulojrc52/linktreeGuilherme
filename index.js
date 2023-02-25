50bbea43fb6a562a4b73c6acc42250f494f62c88
Paulo Júnior <paulojrcosta300@gmail.com>
2023-02-25 13:30:42 -0300

    ultimos ajustes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const containerBoxes = document.querySelector('.containerBox')
 const containerWorks = document.querySelector('.containerWorks')
 
 const boxes = [
-  {title: 'Trabalhos', img: 'images/tatto.jpg', path: './service/index.html', alt: 'Trabalhos'},
+  {title: 'Trabalhos', img: './service/imgservice/polegadaC.PNG', path: './service/index.html', alt: 'Trabalhos'},
   {title: 'Atendimento', img: 'images/atendimento.png', path: './atendimento/index.html', alt: 'Atendimento'},
   {title: 'Localização', img: '', path: '#', alt: ''},
 ]
@@ -13,7 +13,8 @@ const indexMain = () => {
       return containerBoxes.innerHTML += `
         <div class="box">
           <a href=R${path}>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3821.6758382036633!2d-49.2792617!3d-16.6930972!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef1600b0660f9%3A0xf4ee8be146eb630d!2sTime%207%20Tattoo%20Ink!5e0!3m2!1spt-BR!2sbr!4v1676913359839!5m2!1spt-BR!2sbr" width="140" height="140" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3821.6758382036633!2d-49.2792617!3d-16.6930972!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef1600b0660f9%3A0xf4ee8be146eb630d!2sTime%207%20Tattoo%20Ink!5e0!3m2!1spt-BR!2sbr!4v1676913359839!5m2!1spt-BR!2sbr" width="150" height="200" style="border-radius: 39%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <br>
             <p>${title}</p>
           </a>
         </div>
--- service/index.js
+++ service/index.js
@@ -1,18 +1,18 @@
 const containerWorks = document.querySelector('.containerWorks')
 
 const works = [
-  {name: 'Meia Polegada(Colorida)', price:1200, img: './imgservice/meiapC.PNG', size: 13},
-  {name: 'Meia Polegada(Preto & Branco)', price:1000, img: './imgservice/meiapP.PNG', size: 13},
-  {name: 'Membro Passado(Preto & Branco)', price:3300, img: './imgservice/membropP.PNG', size: 53},
-  {name: 'Polegada(Colorida)', price:1700, img: './imgservice/polegadaC.PNG', size: 21},
-  {name: 'Fechamento Maior(Colorida)', price:3300, img: './imgservice/fechamentomC.PNG', size: 38},
-  {name: 'Membro Passado(Colorida)', price:3700, img: './imgservice/membropC.PNG', size: 58},
-  {name: 'Fechamento Maior(Preto & Branco)', price:2300, img: './imgservice/fechamentomP.PNG', size: 38},
-  {name: 'Fechamento Menor(Preto & Branco)', price:1700, img: './imgservice/fechamentomenorP.PNG', size: 28},
-  {name: 'Fechamento Menor(Colorida)', price:2200, img: './imgservice/fechamentomenorC.PNG', size: 28},
-  {name: 'Polegada(Preto & Branco)', price:1400, img: './imgservice/polegadaP.PNG', size: 21},
   {name: 'Flash Tattoo(Preto & Branco)', price:500, img: './imgservice/flashP.PNG', size: 8},
   {name: 'Flash Tattoo(Colorida)', price:700, img: './imgservice/flashC.PNG', size: 8},
+  {name: 'Meia Polegada(Preto & Branco)', price:1000, img: './imgservice/meiapP.PNG', size: 13},
+  {name: 'Meia Polegada(Colorida)', price:1200, img: './imgservice/meiapC.PNG', size: 13},
+  {name: 'Polegada(Preto & Branco)', price:1400, img: './imgservice/polegadaP.PNG', size: 21},
+  {name: 'Polegada(Colorida)', price:1700, img: './imgservice/polegadaC.PNG', size: 21},
+  {name: 'Membro Passado(Preto & Branco)', price:3300, img: './imgservice/membropP.PNG', size: 58},
+  {name: 'Membro Passado(Colorida)', price:3700, img: './imgservice/membropC.PNG', size: 58},
+  {name: 'Fechamento Menor(Preto & Branco)', price:1700, img: './imgservice/fechamentomenorP.PNG', size: 28},
+  {name: 'Fechamento Menor(Colorida)', price:2200, img: './imgservice/fechamentomenorC.PNG', size: 28},
+  {name: 'Fechamento Maior(Preto & Branco)', price:2300, img: './imgservice/fechamentomP.PNG', size: 38},
+  {name: 'Fechamento Maior(Colorida)', price:3300, img: './imgservice/fechamentomC.PNG', size: 38},
 ]
 
 const indexWorks = () => {
