20a8ac540881ad1b5cd929c6efdeb1833de907be
Paulo Júnior <paulojrcosta300@gmail.com>
2023-03-04 13:27:46 -0300

    page service refactoring - toasty

--- atendimento/index.js
+++ atendimento/index.js
@@ -1,65 +1,77 @@
 const form = document.querySelector('.form')
 const toast = document.querySelector('.toast')
 
+const resetForm = () => {
+  scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+  })
+  form.reset()
+}
+
+const toastVisibleOrNot = (
+  response, 
+  success, 
+  error, 
+  classAdd, 
+  classRemove
+  ) => {
+
+  if(response) {
+    toast.textContent = success
+    toast.classList.add(classAdd)
+    toast.classList.remove(classRemove)
+  } else {
+    toast.textContent = error
+    toast.classList.add(classRemove)
+    toast.classList.remove(classAdd)
+  }
+
+  resetForm()
+}   
+
 const isToast = (response) => {
-     
-    if(response) {
-        toast.textContent = 'Mensagem enviada com sucesso!'
-        toast.classList.remove('error')
-        toast.classList.add('visible')
-        scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        })
-        form.reset()   
-    }
+  toastVisibleOrNot(
+    response, 
+    'Mensagem enviada com sucesso!', 
+    'Ops, algo deu errado!', 
+    'visible',
+    'error'
+  )
 
-    if(!response) {
-        toast.textContent = 'Ops, algo deu errado!'
-        toast.classList.remove('visible')
-        toast.classList.add('error')
-        scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        })
-        form.reset()  
-    }
-
-    setTimeout(() => {
-        toast.classList.remove('visible')
-        toast.classList.remove('error')
-    }, 3000)
+  setTimeout(() => {
+    toast.classList.remove('visible')
+    toast.classList.remove('error')
+  }, 3000)
 }
 
 form.addEventListener('submit',(event) => {
-    event.preventDefault()
-    const url = 'https://api.staticforms.xyz/submit'
+  event.preventDefault()
+  const url = 'https://api.staticforms.xyz/submit'
 
-    const accessKey = document.querySelector('#accessKey').value
-    const subject = document.querySelector("#subject").value
-    const name = document.querySelector('#inputName').value
-    const age = document.querySelector('#inputIdade').value
-    const email = document.querySelector('#inputEmail').value
-    const phone = document.querySelector('#inputPhone').value
-    const message = document.querySelector('#texteAreaMensagem').value
+  const accessKey = document.querySelector('#accessKey').value
+  const subject = document.querySelector("#subject").value
+  const name = document.querySelector('#inputName').value
+  const age = document.querySelector('#inputIdade').value
+  const email = document.querySelector('#inputEmail').value
+  const phone = document.querySelector('#inputPhone').value
+  const message = document.querySelector('#texteAreaMensagem').value
 
-
-    fetch(url, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            accessKey: accessKey,
-            subject: subject,
-            name: name,
-            $age: age,
-            email: email,
-            phone: phone,
-            message: message,
-        })
-    }).then((response) => isToast(response.ok))
-      
+  fetch(url, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      accessKey: accessKey,
+      subject: subject,
+      name: name,
+      $age: age,
+      email: email,
+      phone: phone,
+      message: message,
+    })
+  }).then((response) => isToast(response.ok))
 })
+
